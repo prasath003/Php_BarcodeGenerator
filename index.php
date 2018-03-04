0a20def9fbe48321c703707f1c1b730108c28903
Prasath Muthaiyan <prasath.muthaiyan@gmail.com>
2018-03-04 17:45:34 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,7 @@
 <?php
 $barcodeid ='';
-
 require "vendor/autoload.php";
-
 ?>
-
 
 <html>
 <head>
@@ -13,19 +10,17 @@ require "vendor/autoload.php";
 <body>
 <div>
     <?php
+    //Simple loop from number 1 to 15 to generate barcodes dynamically.
 
-    //Simple loop from 12 to 15 to generate barcodes dynamically.
+    for($barcodeid = 1 ; $barcodeid<=15;$barcodeid++){
 
-    for($barcodeid =12 ; $barcodeid<=15;$barcodeid++){
+        echo "Barcode for Number : $barcodeid <br><br>";
 
-        echo "Barcode for Number :$barcodeid <br><br>";
-
-        //This is for png file
+        //This is for png file, you can change yo html,jpg,svg based on your needs with generator change refer the link in readme
         $generator = new Picqer\Barcode\BarcodeGeneratorPNG();
+        
         echo '<img src="data:image/png;base64,' . base64_encode($generator->getBarcode($barcodeid, $generator::TYPE_CODE_128,2,40)) . '"> <hr><br>';
-
     }
-
     ?>
 </div>
 </body>
